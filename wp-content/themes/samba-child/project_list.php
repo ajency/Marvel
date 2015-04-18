75d860e57dcec5691486e1d748d6603fd09f7da8
Renuka <renuka@ajency.in>
2015-04-18 13:19:27 +0530

    small issue in display of project

--- wp-content/themes/samba-child/project_list.php
+++ wp-content/themes/samba-child/project_list.php
@@ -145,37 +145,7 @@ Template Name: Page - Resi Projects List
                             </p>
                         </div>
                     </div>
-                    <div class="single_p_w six columns">
-                        <div class="single_p_img">
-                            <img src="http://lorempixel.com/output/city-q-c-1000-1000-10.jpg">
-                            <div class="single_p_hov_c">
-                                <div class="single_p_likes single_top"><i class="fa fa-heart"></i> 30</div>
-                                <div class="clearfix"></div>
-                                <div class="single_p_info">
-                                    <h6>Office Spaces: 1200 - 50,000 sq. ft.</h6>
-                                    <h6>Retail Spaces: 500 - 1,400 sq. ft.</h6>
-                                </div>
-                                <div class="single_btm">
-                                    <div class="pull-left">
-                                        <a href="#" class="btn_norm single_enq"><i class="fa fa-envelope-o"></i></a>
-                                        <a href="#" class="btn_norm single_share-alt"><i class="fa fa-share"></i></a>
-                                    </div>
-                                    <div class="pull-right">
-                                        <a href="#" class="btn_norm single_know">Know More</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="single_p_cap">
-                            <p class="single_p_inf">
-                                <a href="#">
-                                    <span class="single_p_title">Marvel Edge</span>
-                                    |
-                                    <span class="single_p_location">Viman Nagar</span>
-                                </a>
-                            </p>
-                        </div>
-                    </div>
+                    
                     <!--END single project listing-->
                 </div>
             </div>
