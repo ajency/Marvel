7e760abbebe6c46027ee5319bc75d275f78b19ac
Renuka <renuka@ajency.in>
2015-04-30 14:48:42 +0530

    Services page responsive and modified to fit the PSD

--- wp-content/themes/samba-child/page-services.php
+++ wp-content/themes/samba-child/page-services.php
@@ -124,12 +124,13 @@ Template Name: Page - Services
                             <div class="top-sea">
                                 <button type="submit" class="btn_norm sea"><i class="fa fa-search"></i></button>
                             </div>
+                            <div class="top-note">
+                                <p>Note: Minimum deposit of 10 months has to be given prior to taking flat for rent.</p>
 <!--
-                            <div class="pull-right top-view">
                                 <a href="#" class="top_list current"><i class="fa fa-th-large"></i></a>
                                 <a href="#" class="top_map"><i class="fa fa-map-marker"></i></a>
-                            </div>
 -->
+                            </div>
                         </div>
                     </div>
                     <div class="clearfix"></div>
@@ -143,16 +144,19 @@ Template Name: Page - Services
                 </div>
                 <div class="prk_inner_block vc_row-fluid centered columns forent">
                     <div class="row partintro">
-                        <div class="vc_col-sm-6 wpb_column vc_column_container img_hold">
-                            <div class="wpb_wrapper">
+                        <div class="vc_col-sm-12 wpb_column vc_column_container bgrey">
+                            <div class="wpb_wrapper img_hold">
                                 <div class="clearfix"></div>
                                 <div class="work_cont">
-                                    <img src="http://loremflickr.com/1000/477/building">
+                                    <img src="http://loremflickr.com/1000/457/luxury,house">
+                                    <div class="forent_cap">Sample Flat</div>
                                 </div>
                             </div>
+<!--
                         </div>
-                        <div class="vc_col-sm-6 wpb_column vc_column_container introtext">
-                            <div class="wpb_wrapper">
+                        <div class="vc_col-sm-6 wpb_column vc_column_container ">
+-->
+                            <div class="wpb_wrapper introtext">
                                 <div class="clearfix"></div>
                                 <div class="work_cont">
                                     <a href="#" class="proj_title">
@@ -168,105 +172,147 @@ Template Name: Page - Services
                         </div>
                     </div>
                     <div class="row list_forent">
-                        <div class="vc_col-sm-6 wpb_column vc_column_container">
+                        <div class="vc_col-sm-12 wpb_column vc_column_container">
                             <div class="wpb_wrapper">
                                 <div class="clearfix"></div>
-                                <div class="work_cont">
+                                <div class="work_cont tab_con">
                                     <div class="top-tab">
-                                        <div class="top_inner">
+                                       <div class="top_inner t_i_head">
                                             <div class="set">
-                                                <small class="clr_lt">Building</small>
-                                                <big>D</big>
-                                            </div>
-                                            <div class="set">
-                                                <small class="clr_lt">Floor</small>
-                                                <big>10</big>
+                                                <small class="clr_lt">Building | Floor</small>
                                             </div>
                                             <div class="set">
                                                 <small class="clr_lt">Area (SQ.FT.)</small>
-                                                <big>2,155</big>
                                             </div>
                                             <div class="set">
                                                 <small class="clr_lt">No. Of Rooms</small>
-                                                <big>3.5 BHK</big>
                                             </div>
-                                        </div>
-                                        <div class="top_rent">
-                                            <small class="clr_lt">Rent (Rs./Month)</small>
-                                            <big>45,000</big><small> - Unfurnished</small>
-                                        </div>
-                                    </div>
-                                    <div class="btm_footer">
-                                        <div class="pull-left">
-                                            <a href="#" class="wpb_button">
-                                                <span class="wpb_button wpb_btn-inverse wpb_regularsize">
-                                                    View Layout
-                                                </span>
-                                            </a>
-                                        </div>
-                                        <div class="pull-right">
-                                            <a href="#" class="wpb_button enq_ico"><span class="wpb_button wpb_btn-inverse wpb_regularsize"></span></a>
-                                        </div>
-                                        <div class="clearfix"></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="vc_col-sm-6 wpb_column vc_column_container">
-                            <div class="wpb_wrapper">
-                                <div class="clearfix"></div>
-                                <div class="work_cont">
-                                    <div class="top-tab">
-                                        <div class="top_inner">
-                                            <div class="set">
-                                                <small class="clr_lt">Building</small>
-                                                <big>D</big>
+                                            <div class="set rent">
+                                                <small class="clr_lt">Rent (Rs./Month)</small>
                                             </div>
                                             <div class="set">
-                                                <small class="clr_lt">Floor</small>
-                                                <big>10</big>
+                                                
+                                            </div>
+                                        </div>
+                                        <div class="top_inner t_i_body">
+                                            <div class="set">
+                                                <big>D 10</big>
                                             </div>
                                             <div class="set">
-                                                <small class="clr_lt">Area (SQ.FT.)</small>
                                                 <big>2,155</big>
                                             </div>
                                             <div class="set">
-                                                <small class="clr_lt">No. Of Rooms</small>
                                                 <big>3.5 BHK</big>
                                             </div>
+                                            <div class="set rent">
+                                                <big>45,000</big><small> - Unfurnished</small>
+                                            </div>
+                                            <div class="set alrt">
+                                                <a href="#" class="wpb_button enq_ico"><span class="wpb_button wpb_btn-inverse wpb_regularsize"></span></a>
+                                            </div>
                                         </div>
-                                        <div class="top_rent">
-                                            <small class="clr_lt">Rent (Rs./Month)</small>
-                                            <big>45,000</big><small> - Unfurnished</small>
+                                        
+                                        <div class="top_inner t_i_body">
+                                            <div class="set">
+                                                <big>D 10</big>
+                                            </div>
+                                            <div class="set">
+                                                <big>2,155</big>
+                                            </div>
+                                            <div class="set">
+                                                <big>3.5 BHK</big>
+                                            </div>
+                                            <div class="set rent">
+                                                <big>45,000</big><small> - Unfurnished</small>
+                                            </div>
+                                            <div class="set alrt">
+                                                <a href="#" class="wpb_button enq_ico"><span class="wpb_button wpb_btn-inverse wpb_regularsize"></span></a>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="btm_footer">
-                                        <div class="pull-left">
-                                            <a href="#" class="wpb_button">
-                                                <span class="wpb_button wpb_btn-inverse wpb_regularsize">
-                                                    View Layout
-                                                </span>
-                                            </a>
-                                        </div>
-                                        <div class="pull-right">
-                                            <a href="#" class="wpb_button enq_ico"><span class="wpb_button wpb_btn-inverse wpb_regularsize"></span></a>
-                                        </div>
-                                        <div class="clearfix"></div>
-                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        
+                        </div>    
                     </div>
                 </div>
-                
-                
-                <!--Careers Bottom content-->
-                <!--Careers Bottom content-->
-                
-                
-                <div class="clearfix"></div>
+                    <div class="clearfix"></div>
                 </div>
+                <div class="prk_inner_block vc_row-fluid centered columns forent">
+                    <div class="row partintro">
+                        <div class="vc_col-sm-12 wpb_column vc_column_container bgrey">
+                            <div class="wpb_wrapper img_hold">
+                                <div class="clearfix"></div>
+                                <div class="work_cont">
+                                    <img src="http://loremflickr.com/1000/457/luxury,house">
+                                    <div class="forent_cap">Sample Flat</div>
+                                </div>
+                            </div>
+<!--
+                        </div>
+                        <div class="vc_col-sm-6 wpb_column vc_column_container ">
+-->
+                            <div class="wpb_wrapper introtext">
+                                <div class="clearfix"></div>
+                                <div class="work_cont">
+                                    <a href="#" class="proj_title">
+                                        <span class="title">Marvel Diva Phase 2</span>
+                                        <span class="divi">|</span>
+                                        <span class="loca">Magarpatta Road</span>
+                                    </a>
+                                    <p class="excerpt">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row list_forent">
+                        <div class="vc_col-sm-12 wpb_column vc_column_container">
+                            <div class="wpb_wrapper">
+                                <div class="clearfix"></div>
+                                <div class="work_cont tab_con">
+                                    <div class="top-tab">
+                                       <div class="top_inner t_i_head">
+                                            <div class="set">
+                                                <small class="clr_lt">Building | Floor</small>
+                                            </div>
+                                            <div class="set">
+                                                <small class="clr_lt">Area (SQ.FT.)</small>
+                                            </div>
+                                            <div class="set">
+                                                <small class="clr_lt">No. Of Rooms</small>
+                                            </div>
+                                            <div class="set rent">
+                                                <small class="clr_lt">Rent (Rs./Month)</small>
+                                            </div>
+                                            <div class="set">
+                                                
+                                            </div>
+                                        </div>
+                                        <div class="top_inner t_i_body">
+                                            <div class="set">
+                                                <big>D 10</big>
+                                            </div>
+                                            <div class="set">
+                                                <big>2,155</big>
+                                            </div>
+                                            <div class="set">
+                                                <big>3.5 BHK</big>
+                                            </div>
+                                            <div class="set rent">
+                                                <big>45,000</big><small> - Unfurnished</small>
+                                            </div>
+                                            <div class="set alrt">
+                                                <a href="#" class="wpb_button enq_ico"><span class="wpb_button wpb_btn-inverse wpb_regularsize"></span></a>
+                                            </div>
+                                        </div>                                        
+                                </div>
+                            </div>
+                        </div>    
+                    </div>
+                </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="spacer-25"></div>
               <?php endwhile; /* End loop */ ?>
             <?php 
               if ($show_sidebar) 
