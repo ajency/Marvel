842da9e9cc94bd172318e1bc013fd1242fc0228c
paragajency <parag@ajency.in>
2015-06-27 16:48:21 +0530

    service shortcode

---
+++ wp-content/themes/samba-child/modules/shortcode/services.php
@@ -0,0 +1,226 @@
+<?php 
+function  services_shortcode(){
+
+<!--Careers Bottom content-->
+                <!--Careers Bottom content-->
+                <div class="vc_row-fluid full-width">
+                    <div class="vc_col-sm-12">
+                        <div class="top-dd-c">
+<!--
+                            <div class="top-dd one">
+                                <select id="dd_status">
+                                    <option>Ongoing</option>
+                                    <option>Upcoming</option>
+                                </select>
+                            </div>
+-->
+                            <div class="top-dd two">
+                                <select id="dd_city">
+                                    <option>Pune</option>
+                                </select>
+                            </div>
+                            <div class="top-dd thr">
+                                <select id="dd_locality" disabled>
+                                    <option>Locality</option>
+                                </select>
+                            </div>
+                            <div class="top-dd fou">
+                                <select id="dd_type" disabled>
+                                    <option>No. of Bedrooms</option>
+                                </select>
+                            </div>
+                            <div class="top-sea">
+                                <button type="submit" class="btn_norm sea"><i class="fa fa-search"></i></button>
+                            </div>
+                            <div class="top-note">
+                                <p>Note: Minimum deposit of 10 months has to be given prior to taking flat for rent.</p>
+<!--
+                                <a href="#" class="top_list current"><i class="fa fa-th-large"></i></a>
+                                <a href="#" class="top_map"><i class="fa fa-map-marker"></i></a>
+-->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="prk_inner_block vc_row-fluid centered columns">
+                    <div class="row">
+                        <div class="vc_col-sm-12 wpb_column vc_column_container">
+                            <h5>Residential Projects on Rent in Pune (3)</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="prk_inner_block vc_row-fluid centered columns forent">
+                    <div class="row partintro">
+                        <div class="vc_col-sm-12 wpb_column vc_column_container bgrey">
+                            <div class="wpb_wrapper img_hold">
+                                <div class="clearfix"></div>
+                                <div class="work_cont">
+                                    <img src="http://loremflickr.com/1000/457/luxury,house">
+                                    <div class="forent_cap">Sample Flat</div>
+                                </div>
+                            </div>
+<!--
+                        </div>
+                        <div class="vc_col-sm-6 wpb_column vc_column_container ">
+-->
+                            <div class="wpb_wrapper introtext">
+                                <div class="clearfix"></div>
+                                <div class="work_cont">
+                                    <a href="#" class="proj_title">
+                                        <span class="title">Marvel Diva</span>
+                                        <span class="divi">|</span>
+                                        <span class="loca">Magarpatta Road</span>
+                                    </a>
+                                    <p class="excerpt">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row list_forent">
+                        <div class="vc_col-sm-12 wpb_column vc_column_container">
+                            <div class="wpb_wrapper">
+                                <div class="clearfix"></div>
+                                <div class="work_cont tab_con">
+                                    <div class="top-tab">
+                                       <div class="top_inner t_i_head">
+                                            <div class="set">
+                                                <small class="clr_lt">Building | Floor</small>
+                                            </div>
+                                            <div class="set">
+                                                <small class="clr_lt">Area (SQ.FT.)</small>
+                                            </div>
+                                            <div class="set">
+                                                <small class="clr_lt">No. Of Rooms</small>
+                                            </div>
+                                            <div class="set rent">
+                                                <small class="clr_lt">Rent (Rs./Month)</small>
+                                            </div>
+                                            <div class="set">
+                                                
+                                            </div>
+                                        </div>
+                                        <div class="top_inner t_i_body">
+                                            <div class="set">
+                                                <big>D 10</big>
+                                            </div>
+                                            <div class="set">
+                                                <big>2,155</big>
+                                            </div>
+                                            <div class="set">
+                                                <big>3.5 BHK</big>
+                                            </div>
+                                            <div class="set rent">
+                                                <big>45,000</big><small> - Unfurnished</small>
+                                            </div>
+                                            <div class="set alrt">
+                                                <a href="#" class="wpb_button enq_ico"><span class="wpb_button wpb_btn-inverse wpb_regularsize"></span></a>
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="top_inner t_i_body">
+                                            <div class="set">
+                                                <big>D 10</big>
+                                            </div>
+                                            <div class="set">
+                                                <big>2,155</big>
+                                            </div>
+                                            <div class="set">
+                                                <big>3.5 BHK</big>
+                                            </div>
+                                            <div class="set rent">
+                                                <big>45,000</big><small> - Unfurnished</small>
+                                            </div>
+                                            <div class="set alrt">
+                                                <a href="#" class="wpb_button enq_ico"><span class="wpb_button wpb_btn-inverse wpb_regularsize"></span></a>
+                                            </div>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>    
+                    </div>
+                </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="prk_inner_block vc_row-fluid centered columns forent">
+                    <div class="row partintro">
+                        <div class="vc_col-sm-12 wpb_column vc_column_container bgrey">
+                            <div class="wpb_wrapper img_hold">
+                                <div class="clearfix"></div>
+                                <div class="work_cont">
+                                    <img src="http://loremflickr.com/1000/457/luxury,house">
+                                    <div class="forent_cap">Sample Flat</div>
+                                </div>
+                            </div>
+<!--
+                        </div>
+                        <div class="vc_col-sm-6 wpb_column vc_column_container ">
+-->
+                            <div class="wpb_wrapper introtext">
+                                <div class="clearfix"></div>
+                                <div class="work_cont">
+                                    <a href="#" class="proj_title">
+                                        <span class="title">Marvel Diva Phase 2</span>
+                                        <span class="divi">|</span>
+                                        <span class="loca">Magarpatta Road</span>
+                                    </a>
+                                    <p class="excerpt">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row list_forent">
+                        <div class="vc_col-sm-12 wpb_column vc_column_container">
+                            <div class="wpb_wrapper">
+                                <div class="clearfix"></div>
+                                <div class="work_cont tab_con">
+                                    <div class="top-tab">
+                                       <div class="top_inner t_i_head">
+                                            <div class="set">
+                                                <small class="clr_lt">Building | Floor</small>
+                                            </div>
+                                            <div class="set">
+                                                <small class="clr_lt">Area (SQ.FT.)</small>
+                                            </div>
+                                            <div class="set">
+                                                <small class="clr_lt">No. Of Rooms</small>
+                                            </div>
+                                            <div class="set rent">
+                                                <small class="clr_lt">Rent (Rs./Month)</small>
+                                            </div>
+                                            <div class="set">
+                                                
+                                            </div>
+                                        </div>
+                                        <div class="top_inner t_i_body">
+                                            <div class="set">
+                                                <big>D 10</big>
+                                            </div>
+                                            <div class="set">
+                                                <big>2,155</big>
+                                            </div>
+                                            <div class="set">
+                                                <big>3.5 BHK</big>
+                                            </div>
+                                            <div class="set rent">
+                                                <big>45,000</big><small> - Unfurnished</small>
+                                            </div>
+                                            <div class="set alrt">
+                                                <a href="#" class="wpb_button enq_ico"><span class="wpb_button wpb_btn-inverse wpb_regularsize"></span></a>
+                                            </div>
+                                        </div>                                        
+                                </div>
+                            </div>
+                        </div>    
+                    </div>
+                </div>
+                    <div class="clearfix"></div>
+                </div>
+
+
+}
+?>                
