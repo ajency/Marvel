e59d782a3e140cbfe347e885b7dabcb92fb85a2d
Renukaajency <renuka@ajency.in>
2015-07-24 18:51:34 +0530

    Sticky menu - visible on scroll up but hidden during scroll down

--- wp-content/themes/samba-child/slider.js
+++ wp-content/themes/samba-child/slider.js
@@ -44,6 +44,47 @@ function checkIfInView(element){
 
     //all child js
     $(document).ready(function() {
+        //sliding menu code
+        // Hide Header on on scroll down
+        var didScroll;
+        var lastScrollTop = 0;
+        var delta = 5;
+        var navbarHeight = $('#prk_responsive_menu').outerHeight();
+
+        $(window).scroll(function(event){
+            didScroll = true;
+        });
+
+        setInterval(function() {
+            if (didScroll) {
+                hasScrolled();
+                didScroll = false;
+            }
+        }, 250);
+
+        function hasScrolled() {
+            var st = $(this).scrollTop();
+
+            // Make sure they scroll more than delta
+            if(Math.abs(lastScrollTop - st) <= delta)
+                return;
+
+            // If they scrolled down and are past the navbar, add class .nav-up.
+            // This is necessary so you never see what is "behind" the navbar.
+            if (st > lastScrollTop && st > navbarHeight){
+                // Scroll Down
+                $('#prk_responsive_menu').removeClass('nav-down').addClass('nav-up');
+            } else {
+                // Scroll Up
+                if(st + $(window).height() < $(document).height()) {
+                    $('#prk_responsive_menu').removeClass('nav-up').addClass('nav-down');
+                }
+            }
+
+            lastScrollTop = st;
+        }
+
+
         $fullwidth = window.innerWidth ? window.innerWidth : $(window).width();
         $(window).resize(function() {
             $fullwidth = window.innerWidth ? window.innerWidth : $(window).width();
