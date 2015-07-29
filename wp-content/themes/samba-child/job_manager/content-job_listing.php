4bd6f91a845895a3d5084f62e2e9bce8fa68a826
Renukaajency <renuka@ajency.in>
2015-07-29 13:36:38 +0530

    added plugins to the gitignore and latest edited job_listing.php

--- wp-content/themes/samba-child/job_manager/content-job_listing.php
+++ wp-content/themes/samba-child/job_manager/content-job_listing.php
@@ -1,11 +1,11 @@
 <?php global $post; ?>
 <li <?php job_listing_class(); ?> data-longitude="<?php echo esc_attr( $post->geolocation_lat ); ?>" data-latitude="<?php echo esc_attr( $post->geolocation_long ); ?>">
-	<a href="<?php the_job_permalink(); ?>">
+
 		<?php the_company_logo(); ?>
-		<div class="position">
-			<h3><?php the_title();?></h3>
-			<div class="job-description"><?php echo $post->post_content;?></div>
-			<a href="#" class="wpb_button_a">
+		<div class="position row">
+			<h3 class="vc_col-sm-2 wpb_column vc_column_container job-name"><?php the_title();?></h3>
+		  <div class="job-description vc_col-sm-8 wpb_column vc_column_container"><div class="wpb_wrapper"><p><?php echo $post->post_content;?></p></div></div>
+			<a href="#" class="wpb_button_a vc_col-sm-2 wpb_column vc_column_container">
 				<span class="wpb_button wpb_btn-inverse wpb_regularsize">Apply Now</span>
 			</a>
 			<div class="company">
@@ -16,13 +16,13 @@
 		<div class="location">
 			<?php the_job_location( false ); ?>
 		</div>
-		<ul class="meta">
+		<!--<ul class="meta">
 			<?php do_action( 'job_listing_meta_start' ); ?>
 
 			<li class="job-type <?php echo get_the_job_type() ? sanitize_title( get_the_job_type()->slug ) : ''; ?>"><?php the_job_type(); ?></li>
-			<li class="date"><date><?php printf( __( '%s ago', 'wp-job-manager' ), human_time_diff( get_post_time( 'U' ), current_time( 'timestamp' ) ) ); ?></date></li>
+			<li class="date"><date><?php printf( __( '%s ago', 'wp-job-manager' ), human_time_diff( get_post_time( 'U' ), current_time( 'timestamp' ) ) ); ?></date></li>-->
 
 			<?php do_action( 'job_listing_meta_end' ); ?>
 		</ul>
-	</a>
+
 </li>
