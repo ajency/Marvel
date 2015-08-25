3240d2740d6650fd04b9819eee12e8990a17fd08
Renukaajency <renuka@ajency.in>
2015-08-25 18:47:36 +0530

    Undid form callback

--- wp-content/themes/samba-child/slider.js
+++ wp-content/themes/samba-child/slider.js
@@ -46,21 +46,112 @@ function checkIfInView(element){
     $(document).ready(function() {
 
         $(window).load(function() {
-            if ($('div').hasClass('frm_submit')) {
-                // var timerset;
-                // $('.frm_style_formidable-style.with_frm_style .frm_submit input[type=submit]').click(function() {
-                //     var timecount = 0;
-                //     timerset = setInterval(function() {
-                //         timecount += 1;
-                //         if (timecount > 10) {
-                //             clearInterval(timerset);
-                //             console.log('run again');
-                //         } else {
-                //             loadingcontinforms();
-                //             console.log('contentloaded');
-                //         }
-                //     }, 1000);
+
+
+            setTimeout(function() {
+                if (jQuery('h6').hasClass('ofcspace')) {
+                    jQuery('.ofcspace').each(function() {
+                        if (jQuery(this).text().trim().toLowerCase() == 'office spaces:  -  sq. ft.') {
+                            jQuery(this).text('');
+                        }
+                    });
+                }
+                if (jQuery('h6').hasClass('retspace')) {
+                    jQuery('.retspace').each(function() {
+                        if (jQuery(this).text().trim().toLowerCase() == 'retail spaces:  -  sq. ft.') {
+                            jQuery(this).text('');
+                        }
+                    });
+                }
+            }, 500);
+
+            function loadingcontinforms() {
+                $countries = ["Your Country","Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas"
+                ,"Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands"
+                ,"Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica"
+                ,"Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea"
+                ,"Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana"
+                ,"Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India"
+                ,"Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia"
+                ,"Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania"
+                ,"Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia"
+                ,"New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal"
+                ,"Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles"
+                ,"Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan"
+                ,"Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia"
+                ,"Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","United States Minor Outlying Islands","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+                $htmlcount = '';
+                for (var i = 0; i < $countries.length; i++) {
+                    $htmlcount += '<option value="'+ $countries[i] +'">'+ $countries[i] +'</option>';
+                }
+                if ($('div').hasClass('hascountry-list')) {
+                    $('.hascountry-list').find('select').each(function() {
+                        $(this).html($htmlcount)
+                    });
+                }
+                //trying out custom dropdowns
+                // $("#dd_locality").chosen({disable_search_threshold: 450});
+                // $("#dd_locality").change(function() {
+                //     $("#dd_locality").trigger('chosen:updated');
                 // });
+                //dropdown elipsis
+                setTimeout(function() {
+                    $('select').each(function() {
+                        if (!($(this).prev('div').hasClass('elips-cont'))) {
+                            $(this).before('<div class="elips-cont"></div>');
+                        }
+                    });
+
+                    $('.elips-cont').each(function() {
+                        $(this).text($(this).next('select').find('option:selected').text());
+                    });
+                    $('select').each(function() {
+                        $(this).change(function() {
+                        //$(this).prev('.elips-cont').text($("option:selected", this).text());
+                        $cont = $('.elips-cont').parent('div');
+
+                        setTimeout(function() {
+                            $('.elips-cont').each(function() {
+                                $par = $(this).parent('div');
+                                $(this).text($par.find('select').find('option:selected').text());
+                            });
+                        }, 0.7);
+
+                        });
+                    });
+                }, 0.1);
+            }
+            var stuff = setInterval(function() {
+                if ($('.frm_message').is(':visible')) {
+                    setTimeout(function(){jQuery('.frm_message').hide('slow')}, 5000);
+                    clearInterval(stuff);
+                } else {
+                }
+            }, 0.3);
+            loadingcontinforms();
+            $(document).on('click', '.back_btn', function() {
+                setTimeout(function() {
+                    loadingcontinforms();
+                }, 50);
+            });
+
+
+
+            if ($('div').hasClass('frm_submit')) {
+                var timerset;
+                $('.frm_style_formidable-style.with_frm_style .frm_submit input[type=submit]').click(function() {
+                    var timecount = 0;
+                    timerset = setInterval(function() {
+                        timecount += 1;
+                        if (timecount > 10) {
+                            clearInterval(timerset);
+                            console.log('run again');
+                        } else {
+                            loadingcontinforms();
+                            console.log('contentloaded');
+                        }
+                    }, 1000);
+                });
             }
 
             $(document).on('click', '.elips-cont', function() {
@@ -787,91 +878,91 @@ jQuery(window).load(function() {
  * =======================================================
  * ======================================================= */
 
-function frmThemeOverride_frmAfterSubmit(e,f,b,a) {
-    console.log('formidable form details:');
-    console.log('a: ' + a);
-    console.log('f: ' + f);
-    console.log('e: ' + e);
-    var formid = jQuery(a).find('input[name="form_id"]').val();
-    console.log('id ' + formid);
-    //if(typeof(formid) == 'number'){
-        console.log('form ' + formid + ' has been submitted');
-        afterformidablesubmit();
-    //}
-}
-function afterformidablesubmit() {
-    loadingcontinforms();
-}
+// function frmThemeOverride_frmAfterSubmit(e,f,b,a) {
+//     console.log('formidable form details:');
+//     console.log('a: ' + a);
+//     console.log('f: ' + f);
+//     console.log('e: ' + e);
+//     var formid = jQuery(a).find('input[name="form_id"]').val();
+//     console.log('id ' + formid);
+//     //if(typeof(formid) == 'number'){
+//         console.log('form ' + formid + ' has been submitted');
+//         afterformidablesubmit();
+//     //}
+// }
+// function afterformidablesubmit() {
+//     loadingcontinforms();
+// }
 
-function loadingcontinforms() {
-    var countries = ["Your Country","Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas"
-    ,"Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands"
-    ,"Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica"
-    ,"Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea"
-    ,"Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana"
-    ,"Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India"
-    ,"Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia"
-    ,"Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania"
-    ,"Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia"
-    ,"New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal"
-    ,"Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles"
-    ,"Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan"
-    ,"Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia"
-    ,"Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","United States Minor Outlying Islands","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
-    var htmlcount = '';
-    for (var i = 0; i < countries.length; i++) {
-        htmlcount += '<option value="'+ countries[i] +'">'+ countries[i] +'</option>';
-    }
-    if (jQuery('div').hasClass('hascountry-list')) {
-        jQuery('.hascountry-list').find('select').each(function() {
-            jQuery(this).html(htmlcount)
-        });
-    }
-            //trying out custom dropdowns
-            // jQuery("#dd_locality").chosen({disable_search_threshold: 450});
-            // jQuery("#dd_locality").change(function() {
-            //     jQuery("#dd_locality").trigger('chosen:updated');
-            // });
-            //dropdown elipsis
-            setTimeout(function() {
-                jQuery('select').each(function() {
-                    if (!(jQuery(this).prev('div').hasClass('elips-cont'))) {
-                        jQuery(this).before('<div class="elips-cont"></div>');
-                    }
-                });
+// function loadingcontinforms() {
+//     var countries = ["Your Country","Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas"
+//     ,"Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands"
+//     ,"Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica"
+//     ,"Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea"
+//     ,"Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana"
+//     ,"Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India"
+//     ,"Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia"
+//     ,"Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania"
+//     ,"Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia"
+//     ,"New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal"
+//     ,"Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles"
+//     ,"Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan"
+//     ,"Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia"
+//     ,"Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","United States Minor Outlying Islands","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+//     var htmlcount = '';
+//     for (var i = 0; i < countries.length; i++) {
+//         htmlcount += '<option value="'+ countries[i] +'">'+ countries[i] +'</option>';
+//     }
+//     if (jQuery('div').hasClass('hascountry-list')) {
+//         jQuery('.hascountry-list').find('select').each(function() {
+//             jQuery(this).html(htmlcount)
+//         });
+//     }
+//             //trying out custom dropdowns
+//             // jQuery("#dd_locality").chosen({disable_search_threshold: 450});
+//             // jQuery("#dd_locality").change(function() {
+//             //     jQuery("#dd_locality").trigger('chosen:updated');
+//             // });
+//             //dropdown elipsis
+//             setTimeout(function() {
+//                 jQuery('select').each(function() {
+//                     if (!(jQuery(this).prev('div').hasClass('elips-cont'))) {
+//                         jQuery(this).before('<div class="elips-cont"></div>');
+//                     }
+//                 });
 
-                jQuery('.elips-cont').each(function() {
-                    jQuery(this).text(jQuery(this).next('select').find('option:selected').text());
-                });
-                jQuery('select').each(function() {
-                    jQuery(this).change(function() {
-                    //jQuery(this).prev('.elips-cont').text(jQuery("option:selected", this).text());
-                    var cont = jQuery('.elips-cont').parent('div');
+//                 jQuery('.elips-cont').each(function() {
+//                     jQuery(this).text(jQuery(this).next('select').find('option:selected').text());
+//                 });
+//                 jQuery('select').each(function() {
+//                     jQuery(this).change(function() {
+//                     //jQuery(this).prev('.elips-cont').text(jQuery("option:selected", this).text());
+//                     var cont = jQuery('.elips-cont').parent('div');
 
-                    setTimeout(function() {
-                        jQuery('.elips-cont').each(function() {
-                            var par = jQuery(this).parent('div');
-                            jQuery(this).text(par.find('select').find('option:selected').text());
-                        });
-                    }, 0.7);
+//                     setTimeout(function() {
+//                         jQuery('.elips-cont').each(function() {
+//                             var par = jQuery(this).parent('div');
+//                             jQuery(this).text(par.find('select').find('option:selected').text());
+//                         });
+//                     }, 0.7);
 
-                });
-                });
-            }, 0.1);
+//                 });
+//                 });
+//             }, 0.1);
 
-    var stuff = setInterval(function() {
-        if (jQuery('.frm_message').is(':visible')) {
-            setTimeout(function(){jQuery('.frm_message').hide('slow')}, 5000);
-            clearInterval(stuff);
-        } else {
-        }
-    }, 0.3);
-}
-jQuery(document).ready(function() {
-    loadingcontinforms();
-    jQuery(document).on('click', '.back_btn', function() {
-        setTimeout(function() {
-            loadingcontinforms();
-        }, 50);
-    });
-});
+//     var stuff = setInterval(function() {
+//         if (jQuery('.frm_message').is(':visible')) {
+//             setTimeout(function(){jQuery('.frm_message').hide('slow')}, 5000);
+//             clearInterval(stuff);
+//         } else {
+//         }
+//     }, 0.3);
+// }
+// jQuery(document).ready(function() {
+//     loadingcontinforms();
+//     jQuery(document).on('click', '.back_btn', function() {
+//         setTimeout(function() {
+//             loadingcontinforms();
+//         }, 50);
+//     });
+// });
