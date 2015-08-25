4122067ee8ebb3289ed52d104cfca6dff31a2015
parag <parag@ajency.in>
2015-08-25 17:56:37 +0530

    disable drag on commercial

--- wp-content/themes/samba-child/dev/js/views/ProjectListMainView.js
+++ wp-content/themes/samba-child/dev/js/views/ProjectListMainView.js
@@ -226,11 +226,16 @@
                 //jQuery('.right_container').html(mainViewtemplate()); 
                 var current_post_type = this.post_type;
 
-                jQuery('#post_type').val('residential-property');
+                //jQuery('#post_type').val('residential-property');
+                jQuery('#post_type').val(current_post_type);
                             
                 jQuery('#main').html(mainViewtemplate({post_type:current_post_type})); 
 
-                this.make_div_dropable(".drag_area")
+                //this.make_div_dropable(".drag_area")
+                if(current_post_type == 'residential-property'){
+                    this.make_div_dropable(".drag_area")
+                }
+
             },
 
             /*show_compare2:function(){
